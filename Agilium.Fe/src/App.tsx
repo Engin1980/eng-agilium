1498d4eff2b6d895d2a291fa6dc7d91ae4bc1f4c
Marek Vajgl <engin@seznam.cz>
2026-03-17 18:48:05 +0100

    FE: added tailwind

--- Agilium.Fe/src/App.tsx
+++ Agilium.Fe/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -22,11 +22,12 @@ function App() {
           </p>
         </div>
         <button
-          className="counter"
+          className="counter color-blue-300"
           onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
         </button>
+        <h1 className="text-3xl font-bold underline text-violet-300">Hello world!</h1>
       </section>
 
       <div className="ticks"></div>
@@ -115,7 +116,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
