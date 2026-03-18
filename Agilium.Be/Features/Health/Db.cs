87362e5a2eb6d9c629fdf9df88b257b28368ff64
Marek Vajgl <engin@seznam.cz>
2026-03-18 17:16:11 +0100

    BE: Add health endpoints

---
+++ Agilium.Be/Features/Health/Db.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Eng.Agilium.Be.Exceptions;
+using Eng.Agilium.Be.Model.Db;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eng.Agilium.Be.Features.Health.Db;
+
+public record Result(int ResponseTimeMs);
+
+public class Handler(AppDbContext dbContext) : GenericHandler<EmptyCommand, EmptyParameters, Result>
+{
+  public override async Task<Result> HandleAsync(
+    EmptyCommand request,
+    EmptyParameters parameters,
+    CancellationToken cancellationToken
+  )
+  {
+    var sw = Stopwatch.StartNew();
+    try
+    {
+      await dbContext.Database.ExecuteSqlRawAsync("SELECT 1", cancellationToken);
+    }
+    catch (Exception ex)
+    {
+      throw new ServiceUnreachableException(ServiceType.Database, ex);
+    }
+    finally
+    {
+      sw.Stop();
+    }
+
+    return new Result((int)sw.ElapsedMilliseconds);
+  }
+}
+
+[EndpointSummary("Checks if the database is reachable and measures the response time.")]
+public class EndPoint : GenericOkEndpoint<EmptyCommand, EmptyParameters, Handler, Result>
+{
+  public override HttpMethod Method => HttpMethod.Get;
+
+  public override BaseRoute BaseRoute => BaseRoute.Health;
+
+  public override string EndpointRoute => "/db";
+
+  public override string[] RequiredRoles => [];
+}
---
+++ Agilium.Be/Features/Health/Ping.cs
@@ -0,0 +1,24 @@
+﻿namespace Eng.Agilium.Be.Features.Health.Ping;
+public class Handler : GenericHandler<EmptyCommand, EmptyParameters, EmptyResult>
+{
+  public override Task<EmptyResult> HandleAsync(
+    EmptyCommand request,
+    EmptyParameters parameters,
+    CancellationToken cancellationToken
+  )
+  {
+    return Task.FromResult(new EmptyResult());
+  }
+}
+
+[EndpointSummary("Checks if the backend is reachable.")]
+public class Endpoint : GenericOkEndpoint<EmptyCommand, EmptyParameters, Handler, EmptyResult>
+{
+  public override HttpMethod Method => HttpMethod.Get;
+
+  public override BaseRoute BaseRoute => BaseRoute.Health;
+
+  public override string EndpointRoute => "ping";
+
+  public override string[] RequiredRoles => [];
+}
